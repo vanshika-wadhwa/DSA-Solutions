bb59510a80d094304fad64abc16433acc5e9be51
Vanshika Wadhwa <158786226+vanshika-wadhwa@users.noreply.github.com>
2025-01-13 15:20:36 +0530

    Adding Two Sum leetcode problem.

---
+++ TwoSum/README.md
@@ -0,0 +1,6 @@
+# Leetcode problem
+- Id: 1
+- Title: Two Sum
+- Acceptance: 54.6%
+- Difficulty: Easy
+- Link: [Two Sum](https://leetcode.com/problems/two-sum/)
---
+++ TwoSum/TwoSum.cpp
@@ -0,0 +1,14 @@
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    result.push_back(i);
+                    result.push_back(j);
+                    //cout<<'['<<i<<','<<j<<']';
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
